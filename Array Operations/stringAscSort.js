d6f221ce69a630617c8b73d5d80abef5f8d5b5d0
sohailamjad39 <sohailamjad39sgd@gmail.com>
2025-09-03 02:00:24 +0500

    Array Practice: Sort the array of strings in Ascending Order.

---
+++ Array Operations/stringAscSort.js
@@ -0,0 +1,20 @@
+let str = [
+  "Sohail",
+  "Coder",
+  "Developer",
+  "MERN",
+  "Full Stack",
+  "Software Engineer",
+  "NUTECH",
+];
+
+let n = str.length;
+for (let i = 0; i < n; i++) {
+  let minIdx = i;
+  for (let j = i + 1; j < n; j++) {
+    if (str[j] < str[minIdx]) minIdx = j;
+  }
+  [str[i], str[minIdx]] = [str[minIdx], str[i]];
+}
+
+document.write(str);
